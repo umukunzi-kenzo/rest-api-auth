05a98a1d900703690bb8ac36f7864ce6f4a1c293
Kenzo <umukunzicevinkenzo7@gmail.com>
2025-04-16 17:28:14 +0200

    authentication controllers

--- src/db.ts
+++ src/db.ts
@@ -8,7 +8,7 @@ const MONGO_URL = process.env.MONGO_URL as string;
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URL);
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB!');
   } catch (err) {
     console.error(' Connection has failed:', err); 
   }
