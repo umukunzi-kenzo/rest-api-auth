feb90c1bd3bfabd15ba6a7e6f9833b89426826ab
Kenzo <umukunzicevinkenzo7@gmail.com>
2025-04-16 11:16:33 +0200

    Initial project setup: installed dependencies, TypeScript, and Express app structure

---
+++ src/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL as string;
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(' Connection failed:', err); 
+  }
+};
